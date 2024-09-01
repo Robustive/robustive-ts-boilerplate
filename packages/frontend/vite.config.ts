ab68bc7bbdb41947d20e259cf0cba0a83aaaed61
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2024-09-01 14:50:34 +0900

    ローカル（Vite の mode が development）の場合、devTools が利用できるようにした。

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,25 +3,30 @@ import vue from "@vitejs/plugin-vue"
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: "../../",
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    // @see: https://www.npmjs.com/package/vite-plugin-vuetify
-    vuetify()
-  ],
-  resolve: {
-    alias: {
-      "@domain": "/src/domain",
-      "@frontend": "/src/implementation/frontend",
-      "@shared": "/src/implementation/shared",
-      "@presentation": "/src/presentation",
+export default defineConfig(({ mode }) => {
+  return {
+    root: "../../",
+    define: {
+      __VUE_PROD_DEVTOOLS__: mode === "development"
+    },
+    plugins: [
+      vue({
+        template: { transformAssetUrls },
+      }),
+      // @see: https://www.npmjs.com/package/vite-plugin-vuetify
+      vuetify()
+    ],
+    resolve: {
+      alias: {
+        "@domain": "/src/domain",
+        "@frontend": "/src/implementation/frontend",
+        "@shared": "/src/implementation/shared",
+        "@presentation": "/src/presentation",
+      }
+    },
+    build: {
+      outDir: "dist/frontend",
+      sourcemap: true
     }
-  },
-  build: {
-    outDir: "dist/frontend",
-    sourcemap: true
   }
 })
